d4141db346d8466bf78c7c8523714fc087d6838e
unenergizer <unenergizer@gmail.com>
2016-03-06 11:37:03 -0600

    Fixed spectator damage bug.

--- src/com/minepile/mpmgfw/listeners/EntityDamageByEntity.java
+++ src/com/minepile/mpmgfw/listeners/EntityDamageByEntity.java
@@ -23,15 +23,17 @@ public class EntityDamageByEntity implements Listener {
 
 		HashMap<Player, PlayerProfile> playerProfile = PLUGIN.getGameManager().getPlayerProfile();
 		
+		//Lets check for spectator damage.
 		//Check to see if the entity was a player entity.
-		if (event.getDamager() instanceof Player) {
+		if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
 			Player damager = (Player) event.getDamager();
+			Player defender = (Player) event.getEntity();
 			
 			//Make sure we have the player in the PlayerProfile HashMap.
-			if (playerProfile.containsKey(damager)) {
+			if (playerProfile.containsKey(damager) || playerProfile.containsKey(defender)) {
 				
 				//Check if the damager was a spectator.
-				if(playerProfile.get(damager).isSpectator()) {
+				if(playerProfile.get(damager).isSpectator() || playerProfile.get(defender).isSpectator()) {
 					
 					//Cancel the damage if the player was a spectator.
 					event.setCancelled(true);
